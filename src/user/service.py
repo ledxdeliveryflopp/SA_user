b4ca31f88402adbfa108cf8daa150f5529614a15
LedxDeliveryFlopp <korstim18@gmail.com>
2024-05-13 20:30:48 +0700

    add add_avatar func in user service

--- src/user/service.py
+++ src/user/service.py
@@ -1,9 +1,11 @@
+import os
 from dataclasses import dataclass
-from fastapi import Depends
+from fastapi import Depends, File
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from src.settings.depends import get_session
-from src.settings.exceptions import AuthorizationHeaderExceptions, UserDontExistExceptions
+from src.settings.exceptions import AuthorizationHeaderExceptions, UserDontExistExceptions, \
+    BadFileExtensions
 from src.user.models import UserModel
 from src.user.repository import UserRepository
 from src.user.utils import get_token_payload
@@ -30,6 +32,26 @@ class UserService(UserRepository):
         user = await self.find_user_by_name_or_surname(name, surname)
         if not user:
             raise UserDontExistExceptions
+        return user
+
+    async def add_avatar(self, request: Request, avatar: File()) -> UserModel or dict:
+        """Добавление аватара"""
+        header_token = request.headers.get("Authorization")
+        if not header_token:
+            raise AuthorizationHeaderExceptions
+        header_token = header_token.replace("Bearer ", "")
+        token_payload = await get_token_payload(header_token)
+        user = await self.get_user_by_token_payload(token_payload)
+        if not user:
+            raise UserDontExistExceptions
+        with (open(f"static/{token_payload}{avatar.filename}", "wb") as file):
+            avatar_data = await avatar.read()
+            if os.path.splitext(file.name)[1] != '.png' or os.path.splitext(file.name)[1] != '.jpg':
+                raise BadFileExtensions
+            file.write(avatar_data)
+            avatar_url = request.url_for("static", path=f"{token_payload}{avatar.filename}")
+            setattr(user, "avatar", f"{avatar_url}")
+            await self.session_save_object(user)
         return user
 
 
