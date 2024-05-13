245a362238b264614d01554085e0884e1bf577ae
LedxDeliveryFlopp <korstim18@gmail.com>
2024-05-13 20:26:44 +0700

    add static directory

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 from src.user.router import user_router
 
 user_microservice = FastAPI()
 
 
 user_microservice.include_router(user_router)
+
+user_microservice.mount("/static", StaticFiles(directory="static"), name="static")
